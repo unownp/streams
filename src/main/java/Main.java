9f3b0a281dc72e86d412bbec2ab72cb6a583435c
unownp <antiutka@gmail.com>
2023-05-10 22:50:56 +0300

    added examples of methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,49 +1,90 @@
+import org.w3c.dom.ls.LSOutput;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.SQLOutput;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
-    public static final String PATH="src/main/resources/1.txt";
+    public static final String PATH = "src/main/resources/1.txt";
+
     public static void main(String[] args) throws IOException {
-        List<String> list = new ArrayList();
-        list.add("One");
-        list.add("Two");
-        list.add("Three");
-        list.add("Four");
-        list.add("Five");
-        list.add("Six");
-        list.add("Seven");
-        list.add("Eight");
-        list.add("Nine");
-        list.add("Ten");
-        Stream stream = list.stream();
-       // stream.filter(x-> x.toString().length() == 3).forEach(System.out::println);
-        //Stream<String> streamFromFiles = Files.lines(Paths.get(PATH));
-        //streamFromFiles.forEach(System.out::println);
 
-        Person Vasya=new Person();
-        Person Petya=new Person();
+        Person vasya = new Person(29, "Vasya", "Ivanov");
+        Person petya = new Person(39, "Petya", "Ivanov");
+        Person masha = new Person(17, "Masha", "Petrova");
+        Person elena = new Person(52, "Elena", "Alexeeva");
+        Person nikolay = new Person(75, "Nikolay", "Kuznecov");
+        Person ruslan = new Person(12, "Ruslav", "Aliev");
 
-        Vasya.setAge(29);
-        Vasya.setName("Vasya");
-        Vasya.setSurname("Ivanov");
+        List<Person> personList = new ArrayList<>();
+        personList.add(vasya);
+        personList.add(petya);
+        personList.add(masha);
+        personList.add(elena);
+        personList.add(nikolay);
+        personList.add(ruslan);
 
-        Petya.setAge(39);
-        Petya.setName("Petya");
-        Petya.setSurname("Kuznecov");
+        List<Person> personList1 = personList.stream()
+                .map(Person::new).collect(Collectors.toList());
+        personList1.forEach(person -> person.setAge(person.getAge() + 10));
+        System.out.println(personList);
+        System.out.println(personList1);
 
-        List<Person> personList=new ArrayList();
-        List<Person> personListFiltered=new ArrayList();
-        personList.add(Petya);
-        personList.add(Vasya);
-        personList.add(Vasya);
-        Stream<Person> streamPerson=personList.stream();
-        personListFiltered=streamPerson.filter((p)-> p.getAge() < 35
-                && p.getName().contains("V")).distinct().collect(Collectors.toList());
-        System.out.println(personListFiltered);
+        List<Integer> ageList = personList.stream().map(Person::getAge).toList();
+        System.out.println(ageList);
+
+        personList1 = personList1.stream().filter(p -> p.getAge() < 30 && p.getAge() > 20).collect(Collectors.toList());
+        System.out.println(personList1);
+
+        int product = ageList.stream().reduce((accumulator, element) -> (accumulator * element)).get();
+        System.out.println(product);
+
+        personList = personList.stream().sorted(Comparator.comparingInt(Person::getAge)).collect(Collectors.toList());
+        System.out.println(personList);
+
+        //не united и не terminal
+        List<Person> unitedList = Stream.concat(personList.stream(), personList1.stream()).toList();
+        System.out.println(unitedList);
+
+        boolean b1 = unitedList.stream().allMatch(x -> x.getAge() < 50);
+        System.out.println(b1);
+
+        boolean b2 = unitedList.stream().anyMatch(x -> x.getAge() < 0);
+        System.out.println(b2);
+
+        boolean b3 = unitedList.stream().noneMatch(x -> x.getName().startsWith("x"));
+        System.out.println(b3);
+
+        Long l = unitedList.stream().distinct().count();
+        System.out.println(l);
+
+        int min = ageList.stream().max(Comparator.reverseOrder()).get();
+        System.out.println(min);
+
+        unitedList = unitedList.stream().peek(p -> {
+            p.setName(p.getName() + p.getSurname());
+            System.out.print(p.getName() + " ");
+        }).collect(Collectors.toList());
+        System.out.println();
+
+        List<List<Person>> listListPerson = new ArrayList<>();
+        listListPerson.add(unitedList);
+        listListPerson.add(personList);
+        List<Person> flatMappedList = listListPerson.stream().flatMap(Collection::stream).toList();
+        System.out.println(flatMappedList.size());
+
+        Map<Integer, List<Person>> groupMap = flatMappedList.stream().collect(Collectors.groupingBy(Person::getAge));
+        System.out.println(groupMap);
+
+        Map<Boolean, List<Person>> partitionMap = flatMappedList.stream()
+                .collect(Collectors.partitioningBy(e -> e.getSurname().length() < 7));
+        System.out.println(partitionMap);
+
+        Stream<String> streamFromFiles = Files.lines(Paths.get(PATH));
+        streamFromFiles.forEach(System.out::println);
     }
 }
--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,8 +1,16 @@
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Person {
     private int age;
     private String name;
     private String surname;
+
+    //конструктор копирования
+    public Person(Person other) {
+        this(other.age, other.name, other.surname);
+    }
+
 }
